4c1555b53f20e73f61cdd48052ccc27fda0f24ce
ConnerKT <connerthompson121@gmail.com>
2023-05-09 22:48:34 -0500

    Added find method

--- server.js
+++ server.js
@@ -8,36 +8,54 @@ const weatherdata = require("./data/weather.json");
 const port = 3001;
 app.use(cors());
 
-// Our http get is below
+app.get('/', (req, res) => {
+    res.send(`🔴 We're currently live on the Port ${port}✨`)
+})
+// Our http get is below for the endpoint of weather
 app.get('/weather',(req, res) => {
-    let searchQuery = 'Seattle';
+    let searchQuery = req.query.searchQuery;
     let longitudeQuery = req.query.longitudeQuery;
     let latitudeQuery = req.query.latitudeQuery;
-    
-    let seattleData = {
-        name:weatherdata[0].city_name,
-        longitude:weatherdata[0].lon,
-        latitude:weatherdata[0].lat
+    // We set the data for each location into an array
+    // let seattleData = {
+    //     name:weatherdata[0].city_name,
+    //     longitude:weatherdata[0].lon,
+    //     latitude:weatherdata[0].lat
+    // }
+    // let parisData = {
+    //     name:weatherdata[1].city_name,
+    //     longitude:weatherdata[1].lon,
+    //     latitude:weatherdata[1].lat
+    // }
+    // let ammanData = {
+    //     name:weatherdata[2].city_name,
+    //     longitude:weatherdata[2].lon,
+    //     latitude:weatherdata[2].lat
+    // }
+    // let allLocationData = [seattleData,parisData,ammanData];
+    let searchQueryBoolean = weatherdata.find(value => {
+        if (searchQuery == value.city_name || value.lat == latitudeQuery || value.lon == longitudeQuery){
+            res.send("WORKS WORKS")
+            return true;
+        }else {
+            return false
+        }
+    });
+console.log(searchQueryBoolean)
+    if (searchQuery == undefined){
+        res.status(400).send("ERROR please input a city name, latitude, or longitude")
     }
-    let parisData = {
-        name:weatherdata[1].city_name,
-        longitude:weatherdata[1].lon,
-        latitude:weatherdata[1].lat
-    }
-    let ammanData = {
-        name:weatherdata[2].city_name,
-        longitude:weatherdata[2].lon,
-        latitude:weatherdata[2].lat
-    }
-    let allLocationData = [seattleData,parisData,ammanData];
-    let searchQueryBoolean = weatherdata.find(value => value.city_name === searchQuery);
-    if (searchQueryBoolean === true){
-        
-    }
-    
-    
 });
-
+// This is an endpoint for any other endpoint that is not declared, which returns an "Not Found" text
+app.get('*', (request, response) => {
+    response.status(404).send('not found')
+});
 app.listen(port, () => {
     console.log(`🔥 We're live on ${port} 🔥`)
 })
+class Forecast {
+    constructor(date, description) {
+        this.date = date;
+        this.description = description;
+    }
+}
